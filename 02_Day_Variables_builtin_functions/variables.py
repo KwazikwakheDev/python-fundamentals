b2786a7dbc8da485746e566b45214dbb003b5ff1
KwazikwakheDev <thulakwazikwakhe@gmail.com>
2024-06-08 01:43:21 +0200

    This is the initial commit, where i created the variables and also find the type of variable using type() and also few of casting

---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,28 @@
+name = "Kwazikwakhe"
+surname ="Mjuleka"
+age = 280
+height = 1.8
+country = "South Africa"
+province = "KZN"
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+print("\n")
+#Casting
+num_int = 10
+num_float = float(num_int) # converts to a float value
+print(num_float)
+print("\n")
+
+num_str = str(num_int)
+print(num_str)
+print("\n")
+
+num2_str = "10"
+#num2_int = int(num2_str) #converts a string value to an integer
+num3_float = float(num2_str) # converts a string value to an integer
+#print(num2_int)
+print(num3_float) 
+
--- introduction/helloworld.py
+++
@@ -1,7 +0,0 @@
-print("Helloworld")
-
-print(3 + 4) #addition
-print( 4 * 5) #Multiplication
-print(10 // 2) #floor division
-print(10 - 5) # substractio
-print(8 / 4) # division
